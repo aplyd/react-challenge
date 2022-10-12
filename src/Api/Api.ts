8bc68016d56ceee0b64ef34a3a28953e286c24d7
Austin Ftacnik <austin@ftacnik.com>
2022-10-11 23:42:04 -0700

    feat(api): add getBallotData method

--- src/Api/Api.ts
+++ src/Api/Api.ts
@@ -1,8 +1,32 @@
+import { OmdbResponse } from '../types/omdbResponse';
+import { flattenResults } from '../util/flattenResults';
+
 const api = {
-  getBallotData() {
-    return fetch('/api/getBallotData').then((res) => {
-      return res.json();
-    });
+  async getBallotData() {
+    try {
+      const res = await Promise.all([
+        fetch(
+          `http://www.omdbapi.com/?s=null&page=${1}&apikey=${
+            process.env.REACT_APP_OMDB_API_KEY
+          }`
+        ),
+        fetch(
+          `http://www.omdbapi.com/?s=null&page=${2}&apikey=${
+            process.env.REACT_APP_OMDB_API_KEY
+          }`
+        ),
+        fetch(
+          `http://www.omdbapi.com/?s=null&page=${3}&apikey=${
+            process.env.REACT_APP_OMDB_API_KEY
+          }`
+        )
+      ]);
+
+      const data: OmdbResponse[] = await Promise.all(res.map((r) => r.json()));
+      return flattenResults(data);
+    } catch {
+      alert('ballot data failed to load, please reload the page');
+    }
   }
 };
 
