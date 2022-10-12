fdd1bacc377fa34bd6ad4d7a00ccaee4fafb2d8b
Austin Ftacnik <austin@ftacnik.com>
2022-10-12 16:28:38 -0700

    type(util): add return type

--- src/util/flattenResults.ts
+++ src/util/flattenResults.ts
@@ -1,6 +1,6 @@
 import { OmdbResponse, SearchResult } from '../types/omdbResponse';
 
-export const flattenResults = (arr: OmdbResponse[]) => {
+export const flattenResults = (arr: OmdbResponse[]): SearchResult[] => {
   return arr.reduce<SearchResult[]>((acc, curr) => {
     acc.push(...curr.Search);
     return acc;
