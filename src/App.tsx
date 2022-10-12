865288d84f0e5236f7ddc8c949d297a11ce491e6
Austin Ftacnik <austin@ftacnik.com>
2022-10-11 23:55:18 -0700

    feat(ballot): fetch ballot data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import Ballot from './Components/Ballot/Ballot';
 import './App.css';
+import api from './Api/Api';
+import { SearchResult } from './types/omdbResponse';
 
 const App: FC = () => {
+  const [ballot, setBallot] = useState<SearchResult[] | undefined>(undefined);
+
+  useEffect(() => {
+    (async () => {
+      const data = await api.getBallotData();
+      setBallot(data);
+    })();
+  }, []);
+
   return (
     <div className="App">
       <Ballot />
