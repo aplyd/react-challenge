59262bd04d3ddb6f4d982f89401131c8f2bf2828
Austin Ftacnik <austin@ftacnik.com>
2022-10-13 06:49:09 -0700

    refactor(api): hardcode api key for sake of time

--- src/Api/Api.ts
+++ src/Api/Api.ts
@@ -1,25 +1,20 @@
 import { OmdbResponse, SearchResult } from '../types/omdbResponse';
 import { flattenResults } from '../util/flattenResults';
 
+// typically wouldn't do this but it's a free API and easily accessed without a backend anyway ¯\_(ツ)_/¯
+const apiKey = '14ac79b8';
+
 const api = {
   async getBallotData(): Promise<SearchResult[] | undefined> {
     try {
       const res = await Promise.all([
         fetch(
-          `http://www.omdbapi.com/?s=california&page=${1}&apikey=${
-            process.env.REACT_APP_OMDB_API_KEY
-          }`
+          `http://www.omdbapi.com/?s=california&page=${1}&apikey=${apiKey}`
         ),
         fetch(
-          `http://www.omdbapi.com/?s=california&page=${2}&apikey=${
-            process.env.REACT_APP_OMDB_API_KEY
-          }`
+          `http://www.omdbapi.com/?s=california&page=${2}&apikey=${apiKey}`
         ),
-        fetch(
-          `http://www.omdbapi.com/?s=california&page=${3}&apikey=${
-            process.env.REACT_APP_OMDB_API_KEY
-          }`
-        )
+        fetch(`http://www.omdbapi.com/?s=california&page=${3}&apikey=${apiKey}`)
       ]);
 
       const data: OmdbResponse[] = await Promise.all(res.map((r) => r.json()));
