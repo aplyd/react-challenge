3b0cb3ed40d356f71855f0ecd027065e2c335e61
Austin Ftacnik <austin@ftacnik.com>
2022-10-12 23:28:51 -0700

    refactor(title): use css modules

--- src/Components/general/Title/Title.tsx
+++ src/Components/general/Title/Title.tsx
@@ -1,15 +1,15 @@
 import { FC } from 'react';
+import styles from '../../../styles/Title.module.css';
 
 interface TitleProps {
   text: string;
-  className?: string;
   headingLevel: 1 | 2 | 3 | 4 | 5 | 6;
 }
 
-const Title: FC<TitleProps> = ({ text, className, headingLevel = 1 }) => {
+const Title: FC<TitleProps> = ({ text, headingLevel = 1 }) => {
   const Heading: keyof JSX.IntrinsicElements = `h${headingLevel}`;
   return (
-    <div className={className}>
+    <div className={styles.title}>
       <Heading>{text}</Heading>
     </div>
   );
