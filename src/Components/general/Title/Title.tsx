85e23917ed92f9c5118fd76dce7ecdea9bcceea8
Austin Ftacnik <austin@ftacnik.com>
2022-10-13 00:10:34 -0700

    feat(title): apply className prop styles

--- src/Components/general/Title/Title.tsx
+++ src/Components/general/Title/Title.tsx
@@ -1,15 +1,18 @@
+import cx from 'classnames';
 import { FC } from 'react';
 import styles from '../../../styles/Title.module.css';
 
 interface TitleProps {
   text: string;
   headingLevel: 1 | 2 | 3 | 4 | 5 | 6;
+  className?: string;
 }
 
-const Title: FC<TitleProps> = ({ text, headingLevel = 1 }) => {
+const Title: FC<TitleProps> = ({ text, headingLevel = 1, className = '' }) => {
   const Heading: keyof JSX.IntrinsicElements = `h${headingLevel}`;
+
   return (
-    <div className={styles.title}>
+    <div className={cx(styles.title, styles[className])}>
       <Heading>{text}</Heading>
     </div>
   );
