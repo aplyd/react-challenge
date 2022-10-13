278c36e0d719ef6df483f4389356c4afad8f7f32
Austin Ftacnik <austin@ftacnik.com>
2022-10-12 18:52:14 -0700

    feat(ballot): display categories

--- src/Components/Ballot/Ballot.tsx
+++ src/Components/Ballot/Ballot.tsx
@@ -1,7 +1,24 @@
 import { FC } from 'react';
+import { CategorizedBallotData } from '../../types/ballotData';
+import Category from '../Category/Category';
 
-const Ballot: FC = () => {
-  return <div className="ballot">'Your Code Goes Here'</div>;
+interface BallotProps {
+  ballot: CategorizedBallotData[];
+}
+
+const Ballot: FC<BallotProps> = ({ ballot }) => {
+  return (
+    <div className="ballot-container">
+      {ballot.length > 0 &&
+        ballot.map((category) => (
+          <Category
+            category={category.category}
+            nominees={category.items}
+            key={category.id + category.category}
+          />
+        ))}
+    </div>
+  );
 };
 
 export default Ballot;
