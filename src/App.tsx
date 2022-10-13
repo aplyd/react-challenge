5cd8dcd75b3ce30e487d32c8b123bdb4011766b9
Austin Ftacnik <austin@ftacnik.com>
2022-10-12 17:34:05 -0700

    feat(ballot): store categorized ballot data in state

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,21 @@ import { FC, useEffect, useState } from 'react';
 import Ballot from './Components/Ballot/Ballot';
 import './App.css';
 import api from './Api/Api';
-import { SearchResult } from './types/omdbResponse';
+import { categorizeBallotData } from './util/categorizeBallot';
+import { CategorizedBallotData } from './types/ballotData';
 
 const App: FC = () => {
-  const [ballot, setBallot] = useState<SearchResult[] | undefined>(undefined);
+  const [ballot, setBallot] = useState<CategorizedBallotData[] | undefined>(
+    undefined
+  );
 
   useEffect(() => {
     (async () => {
       const data = await api.getBallotData();
-      setBallot(data);
+      if (data) {
+        const ballotData = categorizeBallotData(data);
+        setBallot(ballotData);
+      }
     })();
   }, []);
 
