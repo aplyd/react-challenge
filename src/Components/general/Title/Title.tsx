8ff916bb0ec4df39e54bb1da44e54c51d301a7b1
Austin Ftacnik <austin@ftacnik.com>
2022-10-12 23:13:02 -0700

    feat(title): allow variable heading level

--- src/Components/general/Title/Title.tsx
+++ src/Components/general/Title/Title.tsx
@@ -3,12 +3,14 @@ import { FC } from 'react';
 interface TitleProps {
   text: string;
   className?: string;
+  headingLevel: 1 | 2 | 3 | 4 | 5 | 6;
 }
 
-const Title: FC<TitleProps> = ({ text, className }) => {
+const Title: FC<TitleProps> = ({ text, className, headingLevel = 1 }) => {
+  const Heading: keyof JSX.IntrinsicElements = `h${headingLevel}`;
   return (
     <div className={className}>
-      <h2>{text}</h2>
+      <Heading>{text}</Heading>
     </div>
   );
 };
