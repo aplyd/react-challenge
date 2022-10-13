1c4830455d7885b5b58818b0980dbda50c2a7530
Austin Ftacnik <austin@ftacnik.com>
2022-10-13 04:19:11 -0700

    feat(context): add selection context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { FC, useEffect, useState } from 'react';
+import { FC, useEffect, useState, useCallback, useMemo } from 'react';
 import api from './Api/Api';
-import Ballot from './Components/Ballot/Ballot';
 import './styles/reset.css';
 import './styles/main.css';
+import Ballot from './Components/Ballot/Ballot';
 import Title from './Components/general/Title/Title';
+import { SelectionContext } from './contexts/selectionContext';
 import styles from './styles/App.module.css';
 import { CategorizedBallotData } from './types/ballotData';
 import { categorizeBallotData } from './util/categorizeBallot';
 
 const App: FC = () => {
   const [ballot, setBallot] = useState<CategorizedBallotData[] | []>([]);
+  const [userSelection, setUserSelection] = useState<{ [key: string]: string }>(
+    {}
+  );
 
   useEffect(() => {
     let shouldUpdate = true;
@@ -28,10 +32,23 @@ const App: FC = () => {
     })();
   }, []);
 
+  const chooseSelection = useCallback(
+    (category: string, selection: string) => {
+      setUserSelection({ ...userSelection, [category]: selection });
+    },
+    [userSelection, setUserSelection]
+  );
+
+  const selectionContextValue = useMemo(() => {
+    return { chooseSelection, userSelection };
+  }, [chooseSelection, userSelection]);
+
   return (
     <div className={styles.app}>
       <Title className="app" headingLevel={1} text="Awards 2022" />
-      <Ballot ballot={ballot} />
+      <SelectionContext.Provider value={selectionContextValue}>
+        <Ballot ballot={ballot} />
+      </SelectionContext.Provider>
     </div>
   );
 };
