f3c470644defd01f9347b900aa6a87ce9fb1ceac
Austin Ftacnik <austin@ftacnik.com>
2022-10-13 02:03:29 -0700

    feat(button): add className prop and nominee button styles

--- src/Components/general/Button/Button.tsx
+++ src/Components/general/Button/Button.tsx
@@ -1,13 +1,18 @@
+import cx from 'classnames';
 import { FC } from 'react';
+import styles from '../../../styles/Button.module.css';
 
 interface ButtonProps {
   text: string;
   className?: string;
 }
 
-const Button: FC<ButtonProps> = ({ text, className }) => {
+const Button: FC<ButtonProps> = ({ text, className = '' }) => {
   return (
-    <button className={className} type="button">
+    <button
+      className={cx(styles.button, styles[className], styles.hovered)}
+      type="button"
+    >
       {text}
     </button>
   );
